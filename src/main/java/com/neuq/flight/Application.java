dacae4c2d64ad508a12de4ef25ae681f450083cd
xiaoliufu <xiaoliufu@meituan.com>
2017-04-09 20:06:05 +0800

    init

---
+++ src/main/java/com/neuq/flight/Application.java
@@ -0,0 +1,17 @@
+package com.neuq.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * author: xiaoliufu
+ * date:   2017/4/8.
+ * description:
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
