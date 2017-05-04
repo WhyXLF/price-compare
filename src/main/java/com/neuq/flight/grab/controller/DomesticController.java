d693c9fa9eb832d75cafed1664c88d0b594ff4ab
xiaoliufu <xiaoliufu@meituan.com>
2017-05-04 14:10:50 +0800

    add webmagic

--- src/main/java/com/neuq/flight/grab/controller/DomesticController.java
+++ src/main/java/com/neuq/flight/grab/controller/DomesticController.java
@@ -1,19 +1,28 @@
 package com.neuq.flight.grab.controller;
 
+import com.neuq.flight.grab.entity.common.PriceResult;
+import com.neuq.flight.grab.service.ctrip.CtripGrabService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
 
 /**
  * author: xiaoliufu
  * date:   2017/4/9.
  * description:
  */
+@Slf4j
 @Controller
 @RequestMapping(value = "/domestic")
 public class DomesticController {
     @RequestMapping
-    public String domesticView(){
+    public String domesticView() {
         return "domestic-flight";
     }
 }
--- src/main/java/com/neuq/flight/grab/controller/InternationalController.java
+++ src/main/java/com/neuq/flight/grab/controller/InternationalController.java
@@ -1,7 +1,14 @@
 package com.neuq.flight.grab.controller;
 
+import com.neuq.flight.grab.entity.common.PriceResult;
+import com.neuq.flight.grab.service.ctrip.CtripGrabService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * author: xiaoliufu
@@ -11,8 +18,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/international")
 public class InternationalController {
+    @Resource
+    private CtripGrabService ctripGrabService;
     @RequestMapping
     public String internationalView(){
         return "international-flight";
+    }
+
+    @RequestMapping(value = "/search")
+    @ResponseBody
+    public String domesticSearch(@RequestParam("dep") String fromCityCode, @RequestParam("arr") String toCityCode, @RequestParam("goTime") String goDate, Model model){
+        PriceResult priceResult = ctripGrabService.getSimpleGrabSearchInfoOW(fromCityCode, toCityCode, goDate);
+        model.addAttribute("priceResult",priceResult);
+        return "domestic-flight";
     }
 }
